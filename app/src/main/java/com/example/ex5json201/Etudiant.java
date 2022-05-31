31be1eae17f7037d0b096fa276afd0b74c351fcc
lamnaourm <mohammed.lamnaour@gmail.com>
2022-05-31 17:04:54 +0100

    la classe Etudiant.java

---
+++ app/src/main/java/com/example/ex5json201/Etudiant.java
@@ -0,0 +1,17 @@
+package com.example.ex5json201;
+
+import java.io.Serializable;
+
+public class Etudiant implements Serializable {
+
+    private String ident;
+    private String nom;
+    private String ville;
+    private String naissance;
+    private double noteFr;
+    private double noteHist;
+    private double noteMath;
+    private double notePhys;
+
+
+}
