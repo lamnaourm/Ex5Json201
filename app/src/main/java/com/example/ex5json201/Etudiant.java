4bd9b37b13506705130d306755c32334499b2de2
lamnaourm <mohammed.lamnaour@gmail.com>
2022-06-01 14:36:10 +0100

    la classe Etudiant.java

--- app/src/main/java/com/example/ex5json201/Etudiant.java
+++ app/src/main/java/com/example/ex5json201/Etudiant.java
@@ -7,11 +7,97 @@ public class Etudiant implements Serializable {
     private String ident;
     private String nom;
     private String ville;
+    private String genre;
     private String naissance;
     private double noteFr;
     private double noteHist;
     private double noteMath;
     private double notePhys;
 
+    public Etudiant() {
+    }
 
+    public Etudiant(String ident, String nom, String ville, String genre, String naissance, double noteFr, double noteHist, double noteMath, double notePhys) {
+        this.ident = ident;
+        this.nom = nom;
+        this.ville = ville;
+        this.genre = genre;
+        this.naissance = naissance;
+        this.noteFr = noteFr;
+        this.noteHist = noteHist;
+        this.noteMath = noteMath;
+        this.notePhys = notePhys;
+    }
+
+    public String getIdent() {
+        return ident;
+    }
+
+    public void setIdent(String ident) {
+        this.ident = ident;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getVille() {
+        return ville;
+    }
+
+    public void setVille(String ville) {
+        this.ville = ville;
+    }
+
+    public String getNaissance() {
+        return naissance;
+    }
+
+    public void setNaissance(String naissance) {
+        this.naissance = naissance;
+    }
+
+    public double getNoteFr() {
+        return noteFr;
+    }
+
+    public void setNoteFr(double noteFr) {
+        this.noteFr = noteFr;
+    }
+
+    public double getNoteHist() {
+        return noteHist;
+    }
+
+    public void setNoteHist(double noteHist) {
+        this.noteHist = noteHist;
+    }
+
+    public double getNoteMath() {
+        return noteMath;
+    }
+
+    public void setNoteMath(double noteMath) {
+        this.noteMath = noteMath;
+    }
+
+    public double getNotePhys() {
+        return notePhys;
+    }
+
+    public void setNotePhys(double notePhys) {
+        this.notePhys = notePhys;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
--- app/src/main/java/com/example/ex5json201/MainActivity.java
+++ app/src/main/java/com/example/ex5json201/MainActivity.java
@@ -3,12 +3,113 @@ package com.example.ex5json201;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.SimpleAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Etudiant> etudiants;
+    Spinner sp;
+    TextView t1, t2, t3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etudiants = getAlletudiants();
+        sp = findViewById(R.id.sp);
+        t1 = findViewById(R.id.nom);
+        t2 = findViewById(R.id.ville);
+        t3 = findViewById(R.id.naissance);
+
+        ArrayList<HashMap<String,Object>> list_etds = new ArrayList<>();
+
+        for(Etudiant e:etudiants){
+            HashMap<String, Object> item = new HashMap<>();
+            item.put("nom",e.getNom());
+            if(e.getGenre().equalsIgnoreCase("homme"))
+                item.put("image",R.drawable.homme);
+            else
+                item.put("image",R.drawable.femme);
+            list_etds.add(item);
+        }
+
+        String[] from = {"nom", "image"};
+        int[] to ={R.id.itemNom,R.id.itemImage};
+
+        SimpleAdapter ad = new SimpleAdapter(this,list_etds,R.layout.item_etudiant,from,to);
+        sp.setAdapter(ad);
+
+        sp.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long l) {
+                Etudiant ee = etudiants.get(pos);
+
+                t1.setText(ee.getNom());
+                t2.setText(ee.getVille());
+                t3.setText(ee.getNaissance());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+    }
+
+    public String loadJsonFromRaw(int resID){
+        try {
+            InputStream in = getResources().openRawResource(resID);
+            byte[] data = new byte[in.available()];
+            in.read(data);
+            in.close();
+            return new String(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public ArrayList<Etudiant> getAlletudiants(){
+        ArrayList<Etudiant> res = new ArrayList<>();
+
+        try {
+            JSONArray arr = new JSONArray(loadJsonFromRaw(R.raw.etudiants));
+
+            for(int i=0;i<arr.length();i++){
+                JSONObject obj = arr.getJSONObject(i);
+                Etudiant e = new Etudiant();
+                e.setIdent(obj.getString("ident"));
+                e.setNom(obj.getString("nom"));
+                e.setVille(obj.getString("ville"));
+                e.setGenre(obj.getString("genre"));
+                e.setNaissance(obj.getString("naissance"));
+                e.setNoteFr(obj.getJSONObject("resultat").getDouble("fr"));
+                e.setNoteMath(obj.getJSONObject("resultat").getDouble("math"));
+                e.setNoteHist(obj.getJSONObject("resultat").getDouble("hist"));
+                e.setNotePhys(obj.getJSONObject("resultat").getDouble("phys"));
+
+                res.add(e);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return res;
     }
 }
